ee080029fde44df286867406e5ae49cb09cd5255
Imesh29 <ridrajakaruna29@gmail.com>
2026-01-04 21:15:07 +0530

    Add Products page with sidebar and update navigation
    Introduced ProductsPage and ProductsSidebar components with corresponding styles. Updated App.jsx to render ProductsPage instead of Homepage. Enhanced LinkWithIcon to support sidebar-specific styling.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./components/Home/Homepage";
+import ProductsPage from "./components/Products/ProductsPage";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <main>
-        <Homepage />
+        {/* <Homepage /> */}
+        <ProductsPage />
       </main>
     </div>
   );
---
+++ src/components/Products/ProductsPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./ProductsPage.css";
+import ProductsSidebar from "./ProductsSidebar";
+
+const ProductsPage = () => {
+  return (
+    <section className="products_page">
+      <ProductsSidebar />
+
+      <section className="products_list_section">Products List</section>
+    </section>
+  );
+};
+
+export default ProductsPage;
