e6aa1f4da02952a2e52db1bb789597a1f6b5d41b
Imesh29 <ridrajakaruna29@gmail.com>
2026-01-12 15:43:45 +0530

    Add reusable Table and QuantityInput components and modify the cart  page Component
    Introduced a generic Table component and a QuantityInput component for consistent UI across cart and product pages. Updated CartPage and SingleProductPage to use these new components, and added corresponding CSS for styling. Also added new styles for cart-specific elements.

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 import "./CartPage.css";
+import remove from "../../assets/remove.png";
 import user from "../../assets/user.webp";
+import Table from "../Common/Table";
+import QuantityInput from "../SingleProduct/QuantityInput";
 
 const CartPage = () => {
   return (
@@ -14,7 +17,26 @@ const CartPage = () => {
         </div>
       </div>
 
-      {/* Cart Table */}
+      <Table headings={["Item", "Price", "Quantity", "Total", "Remove"]}>
+        <tbody>
+          <tr>
+            <td>iPhone 14</td>
+            <td>$999</td>
+            <td className="align_center table_quantity_input">
+              <QuantityInput />
+            </td>
+            <td>$999</td>
+            <td>
+              <img
+                src={remove}
+                alt="remove icon"
+                className="cart_remove_icon"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+
       <table className="cart_bill">
         <tbody>
           <tr>
