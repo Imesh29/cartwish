e60663b484d9587d644ba0699c76f20de792ac27
Imesh29 <ridrajakaruna29@gmail.com>
2026-01-30 11:47:30 +0530

    Fetching Categories to SideBar

--- src/components/Products/ProductsSidebar.jsx
+++ src/components/Products/ProductsSidebar.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import apiClients from "../../utils/api-clients";
 import "./ProductsSidebar.css";
 import rocket from "../../assets/rocket.png";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 
 const ProductsSidebar = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClients
+      .get("/category")
+      .then((res) => setCategories(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
+
   return (
     <aside className="products_sidebar">
       <h2>Category</h2>
 
       <div className="categiry_links">
-        <LinkWithIcon
-          title="Electronics"
-          link="products?category=electronics"
-          emoji={rocket}
-          sidebar={true}
-        />
+        {error && <em className="from_error">{error}</em>}
+        {categories.map((category) => (
+          <LinkWithIcon
+            id={category._id}
+            title={category.name}
+            link={`products?category=$(category.name)`}
+            emoji={`http://localhost:5000/category/${category.image}`}
+            sidebar={true}
+          />
+        ))}
       </div>
     </aside>
   );
